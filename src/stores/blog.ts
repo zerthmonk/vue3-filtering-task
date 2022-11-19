3a142ddfc9ed4a376a26f00e1e44371d46bf7dcc
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-19 13:26:23 +0400

    [replace] active filters now contains some additional data for reusability

--- src/stores/blog.ts
+++ src/stores/blog.ts
@@ -1,5 +1,11 @@
 import { FilterEnum } from "@/misc/constants";
-import type { StoreFilter, StoreLabel, AllowedFields } from "@/types/blog";
+import type {
+  Filter,
+  FilterMethod,
+  StoreFilter,
+  StoreLabel,
+  AllowedFields,
+} from "@/types/blog";
 import type { BlogItem } from "@/components/BlogItem.vue";
 import { defineStore } from "pinia";
 import genFakeItems from "@/misc/fakefactory";
@@ -28,9 +34,9 @@ export const useBlogStore = defineStore("blog", {
       // последовательная фильтрация активными фильтрами
       if (Object.keys(this.activeFilters).length > 0) {
         const filtered = Object.values(this.activeFilters).reduce(
-          (acc: BlogItem[], filterMethod: Function, idx: number) => {
+          (acc: BlogItem[], filter: Filter, idx: number) => {
             const target = idx === 0 ? this.itemsValue : acc;
-            return target.filter((item) => filterMethod(item));
+            return target.filter((item) => filter.apply(item));
           },
           []
         );
@@ -64,10 +70,10 @@ export const useBlogStore = defineStore("blog", {
         .then(({ data }) => (this.itemsValue = data))
         .catch((error) => console.log(error));
     },
-    updateFilter(id: string, method: Function) {
+    updateFilter(filter: Filter) {
       // обновление или создание нового фильтра
       const filters = { ...this.activeFilters };
-      filters[id] = method;
+      filters[filter._id] = filter;
       this.activeFilters = filters;
     },
     dropFilter(id: string) {
@@ -81,7 +87,7 @@ export const useBlogStore = defineStore("blog", {
       switch (filterType) {
         case FilterEnum.country:
           return this.getCountryOptions.reduce(
-            (acc: StoreFilter, countryName: string) => {
+            (acc: FilterMethod, countryName: string) => {
               acc[countryName] = this._getFilteringMethod(
                 FilterEnum.country,
                 countryName
@@ -92,7 +98,7 @@ export const useBlogStore = defineStore("blog", {
           );
         case FilterEnum.score:
           return this.getScoreOptions.reduce(
-            (acc: StoreFilter, expression: string) => {
+            (acc: FilterMethod, expression: string) => {
               acc[expression] = this._getFilteringMethod(
                 FilterEnum.score,
                 expression
